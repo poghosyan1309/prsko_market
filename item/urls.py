3c2d90b0780f307857650cc0462c733aa65b0a14
Narek Poghosyan <poghosyanofcourse@gmail.com>
2023-07-08 15:24:57 +0400

    squashed all previous commits

---
+++ item/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name = 'item'
+urlpatterns = [
+    path('new/', views.new, name='new'),
+    path('<int:pk>/', views.detail, name='detail')
+]
