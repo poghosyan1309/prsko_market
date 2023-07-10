7558a6a10f373c421f3e3314dbec65f495d4be0a
Narek Poghosyan <poghosyanofcourse@gmail.com>
2023-07-10 11:51:11 +0400

    added dashboard

--- prsko_market/urls.py
+++ prsko_market/urls.py
@@ -26,6 +26,7 @@ router.register("", views.UserViewSet)
 urlpatterns = [
     path("", include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += router.urls
